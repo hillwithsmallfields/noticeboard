a1592c4deefe1b932afd79211fadde057263987b
John Sturdy <jcg.sturdy@gmail.com>
2018-01-20 21:10:18 +0000

    Start fleshing out the program

---
+++ noticeboard.el
@@ -0,0 +1,53 @@
+;;; noticeboard.el --- interface to noticeboard hardware  -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2018  John Sturdy
+
+;; Author: John Sturdy <john.sturdy@grapeshot.com>
+;; Keywords: hardware
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; 
+
+;;; Code:
+
+(defvar noticeboard-process nil)
+
+(defun noticeboard-talkback (process string)
+  "The monitor function for the noticeboard hardware talkback."
+  (condition-case evar
+      (eval string)
+    (error (message "Problem in noticeboard talkback"))))
+
+(defun connect-to-noticeboard ()
+  "Connect to the hardware control program on the noticeboard."
+  ;; todo: web pages tell me that the process should run as root, but I don't want to become root here.  Is there a group that is allowed GPIO access?
+  (setq noticeboard-process
+	(start-process "noticeboard"
+		       nil
+		       (expand-file-name "~/bin/noticeboard.py")))
+  (set-process-filter noticeboard-process noticeboard-talkback))
+
+(defun noticeboard-extend-keyboard ()
+  "Extend the keyboard."
+  (process-send-string noticeboard-process "extend\n"))
+
+(defun noticeboard-retract-keyboard ()
+  "Retract the keyboard."
+  (process-send-string noticeboard-process "retract\n"))
+
+(provide 'noticeboard)
+;;; noticeboard.el ends here
--- noticeboard.py
+++ noticeboard.py
@@ -2,6 +2,10 @@
 
 # See README.md for details
 
+import select
+import sys
+import time
+
 try:
     import RPi.GPIO as GPIO
 except RuntimeError:
@@ -20,26 +24,37 @@ pin_brightness = 5  # 29
 pin_lamp_left  = 6  # 31
 pin_lamp_right = 13 # 33
 
+lamp_delay = 0.01
+motor_delay = 0.01
+main_loop_delay = 1.0
+
 pwm = None
 
-def PSU(power_state):
-    GPIO.output(pin_psu, GPIO.HIGH if power_state else GPIO.LOW)
+def power_on():
+    GPIO.output(pin_psu, GPIO.HIGH.LOW)
 
-def keyboard(keyboard_state):
-    PSU(True)
-    # todo: move keyboard
+def power_off():
+    GPIO.output(pin_psu, GPIO.LOW)
 
 last_brightness = 0
 
 def lamps(brightness, fade, left, right):
     if brightness > 0 and (left or right):
-        PSU(True)
+        power_on()
         GPIO.output(pin_lamp_left, GPIO.HIGH if left else GPIO.LOW)
         GPIO.output(pin_lamp_right, GPIO.HIGH if right else GPIO.LOW)
-        if fade:
-            # todo: set a step, and put a delay in
-            for b in range(last_brightness, brightness):
-                pwm.ChangeDutyCycle(pin_brightness, b)
+        if fade and brightness != last_brightness:
+            step = brightness - last_brightness
+            if step > 0:
+                while last_brightness < brightness:
+                    pwm.ChangeDutyCycle(pin_brightness, last_brightness)
+                    time.sleep(lamp_delay)
+                    last_brightness += step
+            else:
+                while last_brightness > brightness:
+                    pwm.ChangeDutyCycle(pin_brightness, last_brightness)
+                    time.sleep(lamp_delay)
+                    last_brightness += step
         else:
             pwm.ChangeDutyCycle(pin_brightness, brightness)
     else:
@@ -48,10 +63,46 @@ def lamps(brightness, fade, left, right):
         GPIO.output(pin_lamp_right, GPIO.LOW)
         pwm.ChangeDutyCycle(pin_brightness, 0)
 
+def shine():
+    power_on()
+    lamps(100, True, True, True)
+
+def quench():
+    lamps(0, True, True, True)
+
+def extend():
+    if not GPIO.input(pin_extended):
+        GPIO.output(pin_motor_a, GPIO.LOW)
+        GPIO.output(pin_motor_b, GPIO.HIGH)
+        while not GPIO.input(pin_extended):
+            time.sleep(motor_delay)
+        GPIO.output(pin_motor_b, GPIO.LOW)
+
+def retract():
+    if not GPIO.input(pin_extended):
+        GPIO.output(pin_motor_b, GPIO.LOW)
+        GPIO.output(pin_motor_a, GPIO.HIGH)
+        while not GPIO.input(pin_extended):
+            time.sleep(motor_delay)
+        GPIO.output(pin_motor_a, GPIO.LOW)
+
+actions = {
+    "on": power_on,
+    "off": power_off,
+    "extend": extend,
+    "retract", retract,
+    "shine", shine,
+    "quench", quench
+    }
+
+activation_delay = 2.0
+
 def main():
     global pwm
     GPIO.setmode(GPIO.BCM)
     GPIO.setup(pin_pir, GPIO.IN)
+    GPIO.setup(pin_retracted, GPIO.IN)
+    GPIO.setup(pin_extended, GPIO.IN)
     GPIO.setup(pin_psu, GPIO.OUT, initial=GPIO.LOW)
     GPIO.setup(pin_motor_a, GPIO.OUT, initial=GPIO.LOW)
     GPIO.setup(pin_motor_b, GPIO.OUT, initial=GPIO.LOW)
@@ -59,6 +110,25 @@ def main():
     pwm = GPIO.PWM(pin_brightness, 1000)
     GPIO.setup(pin_lamp_left, GPIO.OUT, initial=GPIO.LOW)
     GPIO.setup(pin_lamp_right, GPIO.OUT, initial=GPIO.LOW)
+    pir_seen_at = None
+    while True:
+        ready, _, _ = select.select([sys.stdin], [], [], main_loop_delay)
+        if sys.stdin in ready:
+            command = sys.stdin.readline()
+            if command in actions:
+                actions[command]()
+            else:
+                print '(error "Unknown noticeboard command" ', command, ')'
+        if GPIO.input(pin_pir):
+            if pir_seen_at:
+                if time.time() > pir_seen_at + activation_delay:
+                    print "(pir_triggered)"
+                    extend()
+                    shine()
+            else:
+                pir_seen_at = time.time()
+        else:
+            pir_seen_at = None
 
 if __name__ == "__main__":
     main()
