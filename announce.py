d67e25452599bcfb3f736399a9d8abe294fcfaf0
John Sturdy <jcg.sturdy@gmail.com>
2021-03-15 20:54:10 +0000

    Some bug fixes, and add reloading

--- announce.py
+++ announce.py
@@ -3,28 +3,45 @@
 import argparse
 import csv
 import datetime
+import os
 import sched
 import talkey                   # https://pypi.org/project/talkey/
 import time
 
+LAST_READ = None
+FILE_NAME = None
+PROG_ARGS = None
+
+def sleep_timedelta(td):
+    time.sleep(td.seconds if isinstance(td, datetime.timedelta) else td)
+
 def announce(tts, text):
     print(text)
     tts.say(text)
+    if os.stat(FILE_NAME).st_mtime > LAST_READ:
+        print("Reloading events file")
+        announce_from_file(FILE_NAME, PROG_ARGS)
 
 def announce_day_slots(time_slots, day, args):
-    s = sched.scheduler(time.time, time.sleep)
+    s = sched.scheduler(datetime.datetime.now, sleep_timedelta)
     t = talkey.Talkey(preferred_languages=[args.language],
                       engine_preference=[args.engine])
     now = datetime.datetime.now()
     for start in sorted(time_slots.keys()):
-        when = datetime.datetime.combine(day,
-                                         start.timestamp())
+        when = datetime.datetime.combine(day, start)
         if when > now:
+            print("scheduling", time_slots[start], "at", when)
             s.enterabs(when, 1,
                        announce, (t, time_slots[start]))
     s.run()
 
 def announce_from_file(inputfile, args):
+    global LAST_READ
+    LAST_READ = os.stat(inputfile).st_mtime
+    global FILE_NAME
+    FILE_NAME = inputfile
+    global PROG_ARGS
+    PROG_ARGS = args
     with open (inputfile) as instream:
         announce_day_slots(
             {datetime.time.fromisoformat(row['Start']): row['Activity']
@@ -32,6 +49,10 @@ def announce_from_file(inputfile, args):
             datetime.date.today(),
             args)
 
+def empty_queue(s):
+    for event in s.queue():
+        s.cancel(event)
+        
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--language', '-l',
