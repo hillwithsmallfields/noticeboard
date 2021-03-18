2fd60042b4d7dc19d866575b63f5db52624b481e
John Sturdy <jcg.sturdy@gmail.com>
2021-03-18 20:57:13 +0000

    Start on merging multiple files

--- announce.py
+++ announce.py
@@ -4,6 +4,7 @@ import argparse
 import calendar
 import csv
 import datetime
+import numbers
 import os
 import sched
 import talkey                   # https://pypi.org/project/talkey/
@@ -12,60 +13,147 @@ import time
 def sleep_timedelta(td):
     time.sleep(td.seconds if isinstance(td, datetime.timedelta) else td)
         
-def announce(announcer, text):
-    print(text)
-    announcer.talker.say(text)
-    if os.stat(announcer.inputfile).st_mtime > announcer.last_read:
-        print("Reloading events file")
+def announce(announcer, slot):
+    print(slot.activity)
+    announcer.talker.say(slot.activity)
+    if any((os.stat(inputfile).st_mtime > lastread
+            for inputfile, lastread in announcer.last_read.items())):
         announcer.empty_queue()
-        announcer.load()
+        for inputfile in announcer.last_read.keys():
+            announcer.load(inputfile)
 
+def as_duration(duration):
+    if isinstance(duration, datetime.timedelta):
+        return duration
+    if isinstance(duration, numbers.Number):
+        return datetime.timedelta(minutes=duration)
+    hours_minutes = duration.split(':')
+    return (datetime.timedelta(minutes=int(hours_minutes[0]))
+            if len(hours_minutes) == 1
+            else datetime.timedelta(hours=int(hours_minutes[-2]), minutes=int(hours_minutes[-1])))
+
+def as_time(when):
+    return (when
+            if isinstance(when, datetime.time)
+            else (when.time()
+                  if isinstance(when, datetime.datetime)
+                  else (datetime.time.fromisoformat(when))))
+
+def as_datetime(when):
+    return (when
+            if isinstance(when, datetime.datetime)
+            else (datetime.datetime.combine(datetime.date.today(),
+                                            when.time())
+                  if isinstance(when, datetime.time)
+                  else (datetime.time.fromisoformat(when))))
+
+class TimeSlot():
+
+    pass
+
+    def __init__(self,
+                 start, activity,
+                 duration=None,
+                 sound=None):
+        self.start = as_datetime(start)
+        self.end = (self.start + as_duration(duration)) if duration else None
+        self.activity = activity
+
+    def __repr__(self):
+        return "<Activity from %s to %s doing %s>" % (self.start, self.end, self.activity)
+
+    def __str__(self):
+        return "<%s--%s: %s>" % (self.start, self.end, self.activity)
+        
+    def in_progress_at(self, when):
+        return when >= self.start and when < self.end
+
+    def clashes_with(self, other):
+        # TODO: I don't think this is right in practice
+        return (self.in_progress_at(other.start)
+                or self.in_progress_at(other.end)
+                or other.in_progress_at(self.start)
+                or other.in_progress_at(self.end))
+        
 class Announcer():
 
     pass
 
-    def __init__(self, inputfile,
-                 engine, language):
-        self.slots = None
+    def __init__(self, engine, language):
         self.engine = engine
         self.language = language
-        self.inputfile = inputfile
-        self.last_read = None
+        self.slots = {}
+        self.last_read = {}
         self.scheduler = sched.scheduler(datetime.datetime.now, sleep_timedelta)
         self.talker = talkey.Talkey(preferred_languages=[self.language],
                                     engine_preference=[self.engine])
-        self.load()
 
-    def load(self):
-        self.last_read = os.stat(self.inputfile).st_mtime
-        print("loading", self.inputfile)
-        with open (self.inputfile) as instream:
-            self.announce_day_slots(
-                {datetime.time.fromisoformat(row['Start']): row['Activity']
-                 for row in csv.DictReader(instream)},
-                datetime.date.today())
+    def load(self, input_file):
+        if input_file is None:
+            return
+        self.last_read[input_file] = os.stat(input_file).st_mtime
+        print("loading", input_file)
+        today = datetime.date.today()
+        with open (input_file) as instream:
+            pending = None
+            incoming = []
+            for row in csv.DictReader(instream):
+                start = datetime.datetime.combine(today, as_time(row['Start']))
+                if pending:
+                    prev_start, prev_activity = pending
+                    prev_duration = as_time(start) - prev_start
+                    incoming.append(TimeSlot(prev_start, prev_activity, prev_duration))
+                    pending = None
+                activity = row['Activity']
+                duration = row.get('Duration')
+                if duration:
+                    incoming.append(TimeSlot(start, activity, duration))
+                else:
+                    pending = (start, activity)
+            if pending:
+                prev_start, prev_activity = pending
+                prev_duration = as_time("23:59") - as_time(prev_start)
+                incoming[prev_start] = TimeSlot(prev_start, prev_activity, prev_duration)
+            print("merging", incoming, "into", self.slots)
+            clashed = set()
+            for what in incoming:
+                print("  merging", what, "into", self.slots)
+                for when, already in self.slots.items():
+                    print("  checking", what, "against prior", already)
+                    if what.clashes_with(already):
+                        print("clash between", what, already)
+                        clashed.add(when)
+                self.slots[what.start] = what
+            for clash in clashed:
+                print("removing clash", self.slots[clash])
+                del self.slots[clash]
 
-    def announce_day_slots(self, time_slots, day):
+    def show(self):
+        for slot in self.slots:
+            print(slot)
+                
+    def schedule_announcements(self):
         now = datetime.datetime.now()
-        for start in sorted(time_slots.keys()):
-            when = datetime.datetime.combine(day, start)
-            if when > now:
-                print("scheduling", time_slots[start], "at", when)
-                self.scheduler.enterabs(when, 1,
-                                        announce, (self, time_slots[start]))
-
-    def run(self):
+        for start, slot in sorted(self.slots.items()):
+            if start > now:
+                print("scheduling", self.slots[start], "at", start)
+                self.scheduler.enterabs(start, 1,
+                                        announce, (self, slot))
         self.scheduler.run()
 
     def empty_queue(self):
         for event in self.scheduler.queue():
             self.scheduler.cancel(event)
 
-def find_dayfile(directory, dayname):
-    for dayfile in [
-            dayname,
-            dayname.lower(),
-            "Timetable", "timetable", "Daily", "daily", "Default", "default"]:
+def find_default_file(directory):
+    for default_file in ["Timetable", "timetable", "Daily", "daily", "Default", "default"]:
+        default_file = os.path.join(directory, default_file + ".csv" )
+        if os.path.isfile(default_file):
+            return default_file
+    return None
+
+def find_day_file(directory, dayname):
+    for dayfile in [dayname, dayname.lower()]:
         dayfile = os.path.join(directory, dayfile + ".csv" )
         if os.path.isfile(dayfile):
             return dayfile
@@ -84,11 +172,18 @@ def main():
     parser.add_argument('inputfile')
     args = parser.parse_args()
 
-    Announcer((find_dayfile(args.inputfile,
-                            calendar.day_name[datetime.datetime.now().weekday()])
-                           if os.path.isdir(args.inputfile)
-                           else args.inputfile),
-              args.engine, args.language).run()
+    my_day = Announcer(args.engine, args.language)
+    if os.path.isdir(args.inputfile):
+        my_day.load(find_default_file(args.inputfile))
+        my_day.load(find_day_file(args.inputfile,
+                                  calendar.day_name[datetime.datetime.now().weekday()]))
+    else:
+        my_day.load(args.inputfile)
+
+    if args.verbose:
+        my_day.show()
+        
+    my_day.schedule_announcements()
 
 if __name__ == '__main__':
     main()
