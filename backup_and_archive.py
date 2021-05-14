16c649524a9e4d8cf458f24a59b216778755bf33
John Sturdy <jcg.sturdy@gmail.com>
2021-05-14 19:08:21 +0100

    Move backup and archive code into here from qs/update

---
+++ backup_and_archive.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python3
+
+import datetime
+import os
+
+def make_tarball(tarball, parent_directory, of_directory):
+    if not os.path.isfile(tarball):
+        command = "tar cz -C %s %s > %s" % (parent_directory, of_directory, tarball)
+        print("backup command is", command)
+        os.system(command)
+
+def backup_and_archive(file_locations):
+    common_backups = file_locations['common-backups']
+    daily_backup_template = file_locations['daily-backup-template']
+    weekly_backup_template = file_locations['weekly-backup-template']
+    today = datetime.date.today()
+    make_tarball(os.path.join(common_backups, daily_backup_template % today.isoformat()),
+                 os.path.expandvars("$COMMON"),
+                 "org")
+    weekly_backup_day = 2    # Wednesday, probably the least likely day to be on holiday and not using the computer
+    if today.weekday() == weekly_backup_day:
+        make_tarball(os.path.join(common_backups, weekly_backup_template % today.isoformat()),
+                     os.path.expandvars("$HOME"), "common")
+    if today.day == 1:
+        backup_isos_directory = file_locations['backup_isos_directory']
+        monthly_backup_name = os.path.join(backup_isos_directory, file_locations['backup-iso-format'] % today.isoformat())
+        if not os.path.isfile(monthly_backup_name):
+            # make_tarball("/tmp/music.tgz", os.path.expandvars("$HOME"), "Music")
+            make_tarball("/tmp/github.tgz",
+                         file_locations['projects-dir'],
+                         file_locations['projects-user'])
+            files_to_backup = [
+                latest_file_matching(os.path.join(common_backups, daily_backup_template % "*")),
+                latest_file_matching(os.path.join(common_backups, weekly_backup_template % "*")),
+                # too large for genisoimage:
+                # "/tmp/music.tgz",
+                "/tmp/github.tgz"]
+            # prepare a backup of my encrypted partition, if mounted
+            if os.path.isdir(os.path.expandvars("/mnt/crypted/$USER")):
+                os.system("backup-confidential")
+            # look for the output of https://github.com/hillwithsmallfields/JCGS-scripts/blob/master/backup-confidential
+            confidential_backup = latest_file_matching("/tmp/personal-*.tgz.gpg")
+            if confidential_backup:
+                files_to_backup.append(confidential_backup)
+                digest = confidential_backup.replace('gpg', 'sha256sum')
+                if os.path.isfile(digest):
+                    files_to_backup.append(digest)
+                sig = digest + ".sig"
+                if os.path.isfile(sig):
+                    files_to_backup.append(sig)
+            print("Time to take a monthly backup of", files_to_backup, "into", monthly_backup_name)
+            os.system("genisoimage -o %s %s" % (monthly_backup_name, " ".join(files_to_backup)))
+            print("made backup in", monthly_backup_name)
--- chores.py
+++ chores.py
@@ -4,6 +4,8 @@ import argparse
 import os
 import sys
 
+import backup_and_archive
+
 my_projects = os.path.dirname(sys.path[0])
 print("my_projects is", my_projects)
 sys.path.append(os.path.join(my_projects, "qs/update"))
@@ -23,11 +25,26 @@ def main():
                         help="""Be more verbose.""")
     args = parser.parse_args()
 
+    file_locations = {
+        # backups and archives
+        'archive': "~/archive",
+        'backup-iso-format': "backup-%s.iso",
+        'backup_isos_directory': "~/isos/backups",
+        'common-backups': "~/common-backups",
+        'daily-backup-template': "org-%s.tgz",
+        'projects-dir': "~/open-projects/github.com",
+        'projects-user': "hillwithsmallfields",
+        'weekly-backup-template': "common-%s.tgz",
+    }
+    file_locations = {k: os.path.expanduser(os.path.expandvars(v))
+                      for k, v in file_locations.items()}
+
     update.updates(args.charts,
                    args.begin,
                    args.end,
                    not args.no_externals,
                    args.verbose)
+    backup_and_archive.backup_and_archive(file_locations)
 
 if __name__ == '__main__':
     main()
