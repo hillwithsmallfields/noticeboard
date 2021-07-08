22d90827eea6fc210b6c21c9684e6e95c194dd78
John Sturdy <jcg.sturdy@gmail.com>
2021-07-08 21:11:47 +0100

    checkpoint while it's working

--- chores.py
+++ chores.py
@@ -12,6 +12,11 @@ sys.path.append(os.path.join(my_projects, "qs/update"))
 import update
 
 def main():
+
+    """Do various daily 'admin' tasks: merge incoming quantification data
+    from various sources, prepare a dashboard page with charts on it, and
+    do some backups."""
+
     parser = argparse.ArgumentParser()
     parser.add_argument("--charts", default=os.path.expanduser("~/private_html/dashboard"),
                         help="""Directory to write charts into.""")
@@ -21,6 +26,8 @@ def main():
                         help="""Latest date to chart.""")
     parser.add_argument("--no-externals", action='store_true',
                         help="""Don't pester external servers""")
+    parser.add_argument("--testing", action='store_true',
+                        help="""Use an alternate directory which can be reset.""")
     parser.add_argument("--verbose", "-v", action='store_true',
                         help="""Be more verbose.""")
     args = parser.parse_args()
@@ -28,7 +35,10 @@ def main():
     update.updates(args.begin,
                    args.end,
                    not args.no_externals,
-                   args.verbose)
+                   verbose=args.verbose,
+                   testing=args.testing)
+
+    # TODO: recursive listing of directories, or tar and md5sum them, to detect damage
 
     backup_and_archive.backup_and_archive()
 
--- update_repos.py
+++ update_repos.py
@@ -1,13 +1,28 @@
 #!/usr/bin/python3
 
+import datetime
 import os
 import sys
 
+LAST_PULLED_FILE = "/tmp/last_pulled"
+# a day, but allow for a bit of clock jitter:
+UPDATE_INTERVAL = 23.5 * 60 * 60
+
+def updated_lately(filename, recentness=3600):
+    now = datetime.datetime.now()
+    recent = (os.path.isfile(filename)
+              and (now - datetime.datetime.fromtimestamp(os.stat(filename).st_mtime)).total_seconds() < recentness)
+    with open(filename, 'w') as f:
+        f.write(now.isoformat() + "\n")
+    return recent
+
 my_projects = os.path.dirname(sys.path[0])
 
-for project in ("noticeboard", "JCGS-emacs", "JCGS-org-mode", "qs"):
-    print("updating", project)
-    os.chdir(os.path.join(my_projects, project))
-    os.system("git pull")
-
-print("updated lifehacking projects")
+if not updated_lately(LAST_PULLED_FILE, UPDATE_INTERVAL):
+    for project in ("noticeboard", "JCGS-emacs", "JCGS-org-mode", "qs"):
+        print("updating", project)
+        os.chdir(os.path.join(my_projects, project))
+        os.system("git pull")
+    print("Updated lifehacking projects")
+else:
+    print("Skipped updating lifehacking projects, as was done within the past period")
