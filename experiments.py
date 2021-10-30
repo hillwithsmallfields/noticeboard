eca721f03ef11dccfdae3a030d5580fd29dbc68e
John Sturdy <jcg.sturdy@gmail.com>
2021-10-30 14:11:59 +0100

    experimenting with the hardware

--- experiments.py
+++ experiments.py
@@ -5,20 +5,23 @@ import argparse
 import RPi.GPIO as GPIO
 import time
 
+PIN = 12
+
 def main():
     parser = argparse.ArgumentParser()
     args = parser.parse_args()
 
     GPIO.setmode(GPIO.BCM)
-    GPIO.setup(18, GPIO.OUT)    # board pin 12 i.e. 6th pin from end away from sockets, on side nearer edge of board
-    pwm = GPIO.PWN(18, 1000)
+    GPIO.setup(PIN, GPIO.OUT)
+    pwm = GPIO.PWM(PIN, 1000)
     pwm.start(0)
     for i in range(30):
         brightness = 0
         for up in (True, False):
-            brightness += 1 if up else -1
-            pwm.ChangeDutyCycle(brightness)
-            sleep(0.01)
+            for step in range(100):
+                brightness += 1 if up else -1
+                pwm.ChangeDutyCycle(brightness)
+                time.sleep(0.01)
     pwm.stop()
 
 if __name__ == '__main__':
