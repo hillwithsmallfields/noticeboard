00c3f5eb02f977fb60ddb83f7f72aac7fc0a9910
John Sturdy <jcg.sturdy@gmail.com>
2021-11-04 20:27:05 +0000

    Move more into the step function

--- noticeboard.py
+++ noticeboard.py
@@ -8,8 +8,10 @@ import datetime
 import functools
 import os
 import picamera
+import process
 import re
 import select
+import shlex
 import sys
 import time
 import yaml
@@ -44,7 +46,8 @@ config = {
         'lamp': 0.01,
         'motor': 0.01,
         'main_loop': 1.0,
-        'activation': 2.0},
+        'pir_delay': 2.0,
+        'step_max': 200},
     'expected_occupancy': {
         # default for a 9-5 worker who stays in at weekends
         'Monday': ["06:00--08:30",
@@ -101,20 +104,40 @@ class NoticeBoardHardware(object):
 
     pass
 
-    def __init__(self):
+    def __init__(self, config):
+        self.config = config
         self.power = False
         self._lamps = [Lamp(pin_lamp_left), Lamp(pin_lamp_right)]
-        self.extending = False
-        self.retracting = False
+        self.keyboard_status = 'unknown'
         self.moving_steps = 0
+        self.pir_seen_at = 0
+        self.porch_pir_seen_at = 0
+        self.audio_process = None
+        self.user_status = 'unknown'
+        self.temperature = None
+        self.camera = picamera.PiCamera()
+        GPIO.setwarnings(False)
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(pin_pir, GPIO.IN)
+        GPIO.setup(pin_porch_pir, GPIO.IN)
+        GPIO.setup(pin_retracted, GPIO.IN)
+        GPIO.setup(pin_temperature, GPIO.IN)
+        GPIO.setup(pin_extended, GPIO.IN)
+        GPIO.setup(pin_psu, GPIO.OUT, initial=GPIO.LOW)
+        GPIO.setup(pin_speaker, GPIO.OUT, initial=GPIO.LOW)
+        GPIO.setup(pin_motor_a, GPIO.OUT, initial=GPIO.LOW)
+        GPIO.setup(pin_motor_b, GPIO.OUT, initial=GPIO.LOW)
+        GPIO.setup(pin_porch_lamp, GPIO.OUT, initial=GPIO.LOW)
+        GPIO.setup(pin_lamp_left, GPIO.OUT, initial=GPIO.LOW)
+        GPIO.setup(pin_lamp_right, GPIO.OUT, initial=GPIO.LOW)
 
-    def power_on(self):
+    def on(self):
         """Switch the 12V power on."""
         print("switching 12V power on")
         GPIO.output(pin_psu, GPIO.LOW)
         self.power = True
 
-    def power_off(self):
+    def off(self):
         """Switch the 12V power off."""
         print("switching 12V power off")
         GPIO.output(pin_psu, GPIO.HIGH)
@@ -144,70 +167,113 @@ class NoticeBoardHardware(object):
 
     def extend(self):
         """Slide the keyboard drawer out."""
-        if self.extended():
+        if self.keyboard_status == 'extended':
             print("keyboard already extended")
         else:
-            if not self.extending:
+            if self.keyboard_status != 'extending':
                 self.moving_steps = 0
             power_on()
-            self.extending = True
-            # step_time = config['delays']['motor']
-            # timeout = float(config['delays']['motor_timeout'])
-            # countdown = int(timeout / step_time)
-            # print("extending keyboard in", countdown, step_time, "time steps")
+            self.keyboard_status = 'extending'
             GPIO.output(pin_motor_a, GPIO.LOW)
             GPIO.output(pin_motor_b, GPIO.HIGH)
-            # while not GPIO.input(pin_extended):
-            #     countdown -= 1
-            #     if countdown <= 0:
-            #         print("timing out on keyboard motion")
-            #         break
-            #     time.sleep(step_time)
-            # GPIO.output(pin_motor_b, GPIO.LOW)
 
     def retract(self):
         """Slide the keyboard drawer back in."""
-        if self.retracted():
+        if self.keyboard_status == 'retracted':
             print("keyboard already retracted")
         else:
-            if not self.retracting:
+            if self.keyboard_status != 'retracting':
                 self.moving_steps = 0
             power_on()
-            self.retracting = True
-            # step_time = config['delays']['motor']
-            # timeout = float(config['delays']['motor_timeout'])
-            # countdown = int(timeout / step_time)
-            # print("retracting keyboard in", countdown, step_time, "time steps")
+            self.keyboard_status = 'retracting'
             GPIO.output(pin_motor_b, GPIO.LOW)
             GPIO.output(pin_motor_a, GPIO.HIGH)
-            # while not GPIO.input(pin_retracted):
-            #     countdown -= 1
-            #     if countdown <= 0:
-            #         print("timing out on keyboard motion")
-            #         break
-            #     time.sleep(config['delays']['motor'])
-            # GPIO.output(pin_motor_a, GPIO.LOW)
+
+    def pir_actions(self):
+        pass
+
+    def porch_pir_actions(self):
+        pass
+
+    def say(self, text):
+        """Pass the text to a TTS system.
+        That goes via this module so we can control the speaker power switch."""
+        GPIO.output(pin_speaker, GPIO.HIGH)
+        # TODO: first check there's no existing process
+        # TODO: maybe we should have a sound queue?
+        self.audio_process = subprocess.Popen(
+            # TODO: fill in params
+        )
+
+    def play(self, music_filename):
+        """Pass a music file to a player.
+        That goes via this module so we can control the speaker power switch."""
+        GPIO.output(pin_speaker, GPIO.HIGH)
+        # TODO: start the player process asynchronously, switch speaker off at end
+        self.audio_process = subprocess.Popen(
+            # TODO: fill in params
+        )
+
+    def photo(self):
+        """Capture a photo and store it with a timestamp in the filename."""
+        image_filename = os.path.join(self.config['camera']['directory'],
+                                      datetime.datetime.now().isoformat()+".jpg")
+        print("taking photo into", image_filename)
+        self.camera.capture(image_filename)
+        # todo: compare with previous photo in series, and drop any that are very nearly the same
 
     def step(self):
+        """Perform one step of any active operations.
+        Returns whether there's anything going on that needs
+        the event loop to run fast."""
+
         for lamp in self._lamps:
             lamp.step()
-        if self.retracting:
-            if self.retracted() or self.moving_steps > STEPMAX:
-                self.retracting = False
+
+        stepmax = self.config['delays']['step_max']
+        if self.keyboard_status == 'retracting':
+            if self.retracted() or self.moving_steps > stepmax:
+                self.keyboard_status = 'retracted'
                 GPIO.output(pin_motor_b, GPIO.LOW)
                 GPIO.output(pin_motor_a, GPIO.LOW)
             else:
                 self.moving_steps += 1
-        if self.extending:
-            if self.extended() or self.moving_steps > STEPMAX:
-                self.extending = False
+        elif self.keyboard_status == 'extending':
+            if self.extended() or self.moving_steps > stepmax:
+                self.keyboard_status = 'extended'
                 GPIO.output(pin_motor_b, GPIO.LOW)
                 GPIO.output(pin_motor_a, GPIO.LOW)
             else:
                 self.moving_steps += 1
-        return (FAST_INTERVAL
-                if self.retracting or self.extending or any(lamp.changing() for lamp in self._lamps)
-                else SLOW_INTERVAL)
+
+        if self.audio_process:
+            process_result = self.audio_process.poll()
+            if process_result:
+                GPIO.output(pin_speaker, GPIO.LOW)
+                self.audio_process = None
+
+        # TODO: read the temperature from pin_temperature into self.temperature
+
+        if GPIO.input(pin_pir):
+            if self.pir_seen_at:
+                if self.pir_seen_at + self.config['delays']['pir_delay'] < time.time():
+                    self.pir_actions()
+            else:
+                self.pir_seen_at = time.time()
+        else:
+            self.pir_seen_at = 0
+
+        if GPIO.input(pin_porch_pir):
+            if self.porch_pir_seen_at:
+                if self.porch_pir_seen_at + self.config['delays']['porch_pir_delay'] < time.time():
+                    self.porch_pir_actions()
+            else:
+                self.porch_pir_seen_at = time.time()
+        else:
+            self.porch_pir_seen_at = 0
+
+        return (self.keyboard_status in ('retracting', 'extending')
+                    or any(lamp.changing() for lamp in self._lamps))
 
     def report(self):
         """Output the status of the noticeboard hardware."""
@@ -216,30 +282,19 @@ class NoticeBoardHardware(object):
         keyboard_retracted = self.retracted()
         print("12V power on:", power)
         print("PIR:", PIR_active)
-        print("Keyboard extended:", keyboard_extended)
-        print("keyboard retracted:", keyboard_retracted)
-        print("expected_at_home():", expected_at_home())
+        print("Keyboard status:", self.keyboard_status)
 
-def at_home():
-    """Tell the system I am at home."""
-    global manual_at_home
-    global manual_away
-    manual_at_home = True
-    manual_away = False
+    def at_home(self):
+        """Tell the system I am at home."""
+        self.user_status = 'home'
 
-def away():
-    """Tell the system I am away."""
-    global manual_at_home
-    global manual_away
-    manual_at_home = False
-    manual_away = True
+    def away(self):
+        """Tell the system I am away."""
+        self.user_status = 'away'
 
-def auto():
-    """Tell the system I'm not telling it whether I'm at home."""
-    global manual_at_home
-    global manual_away
-    manual_at_home = False
-    manual_away = False
+    def auto(self):
+        """Tell the system I'm not telling it whether I'm at home."""
+        self.user_status = 'automatic'
 
 def convert_interval(interval_string):
     """Convert a string giving start and end times into a tuple.
@@ -283,14 +338,7 @@ def handle_possible_intruder():
         logfile.write(datetime.datetime.now().isoformat() + "\n")
     # todo: send a remote notification e.g. email with the picture
 
-def take_photo():
-    """Capture a photo and store it with a timestamp in the filename."""
-    image_filename = os.path.join(config['camera']['directory'], datetime.datetime.now().isoformat()+".jpg")
-    print("taking photo into", image_filename)
-    camera.capture(image_filename)
-    # todo: compare with previous photo in series, and drop any that are very nearly the same
-
-def quit_controller():
+def quit():
     """Tell the main loop to quit."""
     global running
     running = False
@@ -305,19 +353,21 @@ def show_help():
         print(command_name + ' '*(maxlen - len(command_name)), docstring)
 
 actions = {
-    "auto": auto,
-    "away": away,
-    "extend": extend,
-    "help": show_help,
-    "home": at_home,
-    "off": power_off,
-    "on": power_on,
-    "photo": take_photo,
-    "quench": quench,
-    "quit": quit_controller,
-    "report": report,
-    "retract": retract,
-    "shine": shine
+    "auto",
+    "away",
+    "extend",
+    "help",
+    "home",
+    "off",
+    "on",
+    "photo",
+    "play",
+    "quench",
+    "quit",
+    "report",
+    "retract",
+    "say",
+    "shine"
     }
 
 # based on https://stackoverflow.com/questions/3232943/update-value-of-a-nested-dictionary-of-varying-depth
@@ -352,49 +402,28 @@ def main():
     global photographing
     global photographing_duration
     photographing_duration = datetime.timedelta(0, config['camera']['duration'])
-    global camera
-    camera = picamera.PiCamera()
-    GPIO.setwarnings(False)
-    GPIO.setmode(GPIO.BCM)
-    GPIO.setup(pin_pir, GPIO.IN)
-    GPIO.setup(pin_retracted, GPIO.IN)
-    GPIO.setup(pin_extended, GPIO.IN)
-    GPIO.setup(pin_psu, GPIO.OUT, initial=GPIO.LOW)
-    GPIO.setup(pin_motor_a, GPIO.OUT, initial=GPIO.LOW)
-    GPIO.setup(pin_motor_b, GPIO.OUT, initial=GPIO.LOW)
-    global pwm
-    pwm = GPIO.PWM(pin_brightness, 1000) # TODO: change this; the hardware PWM isn't available when using the sound output jack
-    GPIO.setup(pin_lamp_left, GPIO.OUT, initial=GPIO.LOW)
-    GPIO.setup(pin_lamp_right, GPIO.OUT, initial=GPIO.LOW)
-    pir_seen_at = None
-    main_loop_delay = config['delays']['main_loop']
-    activation_delay = config['delays']['activation']
+
+    controller = NoticeBoardHardware(config)
+
     print("noticeboard hardware controller started")
     while running:
-        ready, _, _ = select.select([sys.stdin], [], [], main_loop_delay)
-        if sys.stdin in ready:
-            command = sys.stdin.readline().strip()
-            if command in actions:
-                actions[command]()
-            else:
-                print('(error "Unknown noticeboard command" ', command, ')')
-        if enable_pir and GPIO.input(pin_pir):
-            if pir_seen_at:
-                if time.time() > pir_seen_at + activation_delay:
-                    print("(pir_triggered)")
-                    if expected_at_home():
-                        extend()
-                        shine()
-                    else:
-                        handle_possible_intruder()
-            else:
-                pir_seen_at = time.time()
+        active = controller.step()
+        # if we're stepping through an activity, ignore commands for now:
+        if active:
+            time.sleep(self.config['delays']['motor'])
         else:
-            pir_seen_at = None
-        if photographing:
-            take_photo()
-            if datetime.datetime.now() >= photographing:
-                photographing = False
+            ready, _, _ = select.select([sys.stdin], [], [], main_loop_delay)
+            if sys.stdin in ready:
+                command = shlex.parse(sys.stdin.readline().strip())
+                if command[0] in actions:
+                    getattr(controller, command)(command)
+                else:
+                    print('(error "Unknown noticeboard command" ', command, ')')
+
+        # if photographing:
+        #     take_photo()
+        #     if datetime.datetime.now() >= photographing:
+        #         photographing = False
     print("noticeboard hardware controller stopped")
 
 if __name__ == "__main__":
