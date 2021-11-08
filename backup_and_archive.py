ff22d0e9f5d2071c9b8d2c746f5b730f9400dda8
John Sturdy <jcg.sturdy@gmail.com>
2021-11-08 19:19:00 +0000

    Provide defaults for when the environment variables used are not set.
    Put the backup days into the config file.
    
    Make it runnable as a standalone program.

--- backup_and_archive.py
+++ backup_and_archive.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 
+import argparse
 import datetime
+import glob
 import os
+import time
 
 import lifehacking_config
 
@@ -16,22 +19,37 @@ def make_tarball(tarball, parent_directory, of_directory):
         print("backup command is", command)
         os.system(command)
 
-def backup_and_archive():
+def latest_file_matching(template):
+    files = glob.glob(template)
+    return files and sorted(files, key=os.path.getmtime)[-1]
+
+def backup_and_archive(force=False):
     global CONFIGURATION
     CONFIGURATION = lifehacking_config.load_config()
     common_backups = CONF('backups', 'common-backups')
+    print("common_backups", common_backups)
+    if common_backups == "" or common_backups.startswith("$"):
+        common_backups = os.path.expandvars("$HOME/common-backups")
+        print("common_backups now", common_backups)
     daily_backup_template = CONF('backups', 'daily-backup-template')
     weekly_backup_template = CONF('backups', 'weekly-backup-template')
     today = datetime.date.today()
     make_tarball(os.path.join(common_backups, daily_backup_template % today.isoformat()),
                  os.path.expandvars("$COMMON"),
                  "org")
-    weekly_backup_day = 2    # Wednesday, probably the least likely day to be on holiday and not using the computer
-    if today.weekday() == weekly_backup_day:
+    weekly_backup_day = CONF('backups', 'weekly-backup-day')
+    if not isinstance(weekly_backup_day, int):
+        try:
+            weekly_backup_day = time.strptime(weekly_backup_day, "%A").tm_wday
+        except ValueError:
+            weekly_backup_day = time.strptime(weekly_backup_day, "%a").tm_wday
+    if force or today.weekday() == weekly_backup_day:
         make_tarball(os.path.join(common_backups, weekly_backup_template % today.isoformat()),
                      os.path.expandvars("$HOME"), "common")
-    if today.day == 1:
+    if force or today.day == int(CONF('backups', 'monthly-backup-day')):
         backup_isos_directory = CONF('backups', 'backup_isos_directory')
+        if backup_isos_directory == "" or backup_isos_directory.startswith("$"):
+            backup_isos_directory = os.path.expandvars("$HOME/isos")
         monthly_backup_name = os.path.join(backup_isos_directory, CONF('backups', 'backup-iso-format') % today.isoformat())
         if not os.path.isfile(monthly_backup_name):
             # make_tarball("/tmp/music.tgz", os.path.expandvars("$HOME"), "Music")
@@ -60,3 +78,15 @@ def backup_and_archive():
             print("Time to take a monthly backup of", files_to_backup, "into", monthly_backup_name)
             os.system("genisoimage -o %s %s" % (monthly_backup_name, " ".join(files_to_backup)))
             print("made backup in", monthly_backup_name)
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--force", "-f",
+                        action='store_true',
+                        help="""Make a backup even if it's not backup day.""")
+    args = parser.parse_args()
+
+    backup_and_archive(args.force)
+
+if __name__ == "__main__":
+    main()
