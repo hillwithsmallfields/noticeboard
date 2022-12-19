a77b7165ac9a727b56330b09e99f3486f31131ca
John C. G. Sturdy <jcg.sturdy@gmail.com>
2022-12-19 19:08:21 +0000

    Tidy up passing of command line args

--- chores.py
+++ chores.py
@@ -11,40 +11,45 @@ print("my_projects is", my_projects)
 sys.path.append(os.path.join(my_projects, "qs/update"))
 import update
 
-def main():
-
-    """Do various daily 'admin' tasks: merge incoming quantification data
-    from various sources, prepare a dashboard page with charts on it, and
-    do some backups."""
-
+def get_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument("--charts", default=os.path.expanduser("~/private_html/dashboard"),
+    parser.add_argument("--charts-dir", default=os.path.expanduser("~/private_html/dashboard"),
                         help="""Directory to write charts into.""")
     parser.add_argument("--begin",
                         help="""Earliest date to chart.""")
     parser.add_argument("--end",
                         help="""Latest date to chart.""")
-    parser.add_argument("--no-externals", action='store_true',
-                        help="""Don't pester external servers""")
-    parser.add_argument("--force", action='store_true',
+    parser.add_argument("--read-externals", "-x", action='store_true',
+                        help="""Read data from external servers""")
+    parser.add_argument("--force", "-f", action='store_true',
                         help="""Do the updates even if the files have been updated
                         within the last day.""")
     parser.add_argument("--testing", action='store_true',
                         help="""Use an alternate directory which can be reset.""")
     parser.add_argument("--verbose", "-v", action='store_true',
                         help="""Be more verbose.""")
-    args = parser.parse_args()
+    return vars(parser.parse_args())
 
-    update.updates(args.begin,
-                   args.end,
-                   not args.no_externals,
-                   verbose=args.verbose,
-                   force=args.force,
-                   testing=args.testing)
+def chores(charts_dir,
+           begin, end,
+           read_externals,
+           verbose, force, testing):
+
+    """Do various daily 'admin' tasks: merge incoming quantification data
+    from various sources, prepare a dashboard page with charts on it, and
+    do some backups."""
+
+    update.updates(charts_dir,
+                   begin,
+                   end,
+                   read_externals,
+                   verbose=verbose,
+                   force=force,
+                   testing=testing)
 
     # TODO: recursive listing of directories, or tar and md5sum them, to detect damage
 
     backup_and_archive.backup_and_archive()
 
 if __name__ == '__main__':
-    main()
+    chores(**get_args())
