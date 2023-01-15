d2701bb98c833edd070b0a19712a51f5cccd512e
John Sturdy <jcg.sturdy@gmail.com>
2023-01-15 15:35:32 +0000

    Add begin and end args

--- playsound.py
+++ playsound.py
@@ -1,26 +1,37 @@
 #!/usr/bin/env python3
 
 import argparse
+import subprocess
 
 import RPi.GPIO as GPIO
 
 import pins
 
-def sounds(on=False, off=False, soundfile=None):
+def sounds(on=False, off=False,
+           begin=None,
+           end=None,
+           soundfile=None):
     GPIO.setwarnings(False)
     GPIO.setmode(GPIO.BCM)
     GPIO.setup(pins.PIN_SPEAKER, GPIO.OUT, initial=GPIO.LOW)
 
     if on:
-        GPIO.output(pins.PIN_SPEAKER, GPIO.HIGH)
-    if off:
         GPIO.output(pins.PIN_SPEAKER, GPIO.LOW)
+    if soundfile:
+        subprocess.run((["ogg123"]
+                        + (["-k", str(begin)] if begin else [])
+                        + (["-K", str(end)] if end else [])
+                        + [soundfile]))
+    if off:
+        GPIO.output(pins.PIN_SPEAKER, GPIO.HIGH)
 
 def get_args():
     parser = argparse.ArgumentParser()
     parser.add_argument("--on", action='store_true')
     parser.add_argument("--off", action='store_true')
-    parser.add_argument("--soundfile"=None)
+    parser.add_argument("--begin", type=float)
+    parser.add_argument("--end", type=float)
+    parser.add_argument("--soundfile")
     return vars(parser.parse_args())
 
 if __name__ == "__main__":
