b441af85eb28a59868ab0436d774044a34da1920
John Sturdy <jcg.sturdy@gmail.com>
2026-01-26 20:10:00 +0000

    small corrections

--- archive.py
+++ archive.py
@@ -9,15 +9,16 @@ import managed_directory
 def archive(directory, 
             archives,
             archives_size):
-    archive_directory = os.path.join(os.path.expandvars(archives),
+    archive_directory = os.path.expandvars(archives)
+    archive_filename = os.path.join(archive_directory,
                                      (os.path.basename(directory)
                                       + "-"
                                       + (datetime.datetime.now(timespec='seconds')
                                          .replace(':', '-')
                                          .replace('T', '-'))
                                       + ".tgz"))
-    subprocess.run(["tar", "-czf",
-                    archive_directory,
+    subprocess.run(["tar", "czf",
+                    archive_filename,
                     os.path.expandvars(directory)])
     managed_directory.trim_directory(archive_directory,
                                      archives_size)
